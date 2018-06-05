cbecc00af3e0f5eab7b04790778be21b5b3437f5
percussion1313 <peterjacobgrant@gmail.com>
2018-06-05 11:43:50 -0500

    Changed first version of function using concise syntax

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-
-function favMovie(movie = 'The Room', name = 'The World') {
-    console.log(`my name is ${name} and my favorite movie is ${movie}`);
+// Template Literals and Default Parameters Value
+/*function favMovie(movie = 'The Room', name = 'The World') {
+    console.log(`My name is ${name} and my favorite movie is ${movie}`);
 }
+favMovie();*/
+
+//Arrow Functions
+
+//Concise version
+favMovie = (name ='World', movie = 'The Room') => console.log(`My name is ${name} and my favorite movie is ${movie}`);
+
 
 favMovie();
+
+
+
