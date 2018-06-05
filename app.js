52b754ec69f48df6ccf555fc0a38fc4c682cf348
percussion1313 <peterjacobgrant@gmail.com>
2018-06-05 12:30:02 -0500

    Added object literal with 2 parameter function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ favMovie();*/
 //Arrow Functions
 
 //Concise version
-favMovie = (name ='World', movie = 'The Room') => console.log(`My name is ${name} and my favorite movie is ${movie}`);
+favMovie = (name = 'World', movie = 'The Room') => console.log(`My name is ${name} and my favorite movie is ${movie}`);
 
 
 favMovie();
@@ -15,12 +15,21 @@ favMovie();
 //First Name Function 1
 let getFirstName = (fullName) => {
     return fullName.split(' ')[0]
-}; 
+};
 console.log(getFirstName('Peter Grant'));
 
 //Concise Version
-let getFirstNameTwo = (fullName) => fullName.split(' ');
+let getFirstNameTwo = (fullName) => fullName.split(' ')[0];
+
 console.log(getFirstNameTwo('Peter Grant'));
+
+//Returning an object literal with two parameters 
+let total = ((p1, p2) => ({
+    squared: Math.pow(p1 + p2, 2),
+    product: p1 * p2}
+))(3,6);
+
+console.log(`${total.squared} ${total.product}`)
 
 
 
